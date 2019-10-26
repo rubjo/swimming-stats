fafd88c0033f4788fae6acfc06e9e926135f7b53
Rune Bjørnerås <rune@proweb.no>
2019-10-26 18:59:39 +0200

    Get gh-pages up and running

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/victor-mono/'
+    : '/',
+  devServer: {
+    compress: true,
+    disableHostCheck: true
+  },
   chainWebpack: config => {
     if (process.env.NODE_ENV === 'development') {
       config
