310244e8450d1f0178f4d56aa54db772c8c6b277
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-12 08:48:57 +0000

    Deploy rubjo/swimming-stats to github.com/rubjo/swimming-stats.git:gh-pages

--- precache-manifest.57843536b67f4d2adb74594a9ff82043.js
+++ precache-manifest.57843536b67f4d2adb74594a9ff82043.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "0e3314497b97e4281821",
-    "url": "/swimming-stats/js/about.3c46cbfa.js"
+    "revision": "98df1726e5c9db40a70b",
+    "url": "/swimming-stats/js/about.2bc5e8c7.js"
   },
   {
-    "revision": "b8c8f7060ebd7eff4e43",
+    "revision": "8f9c67820ed060c97ddd",
     "url": "/swimming-stats/css/app.7354425e.css"
   },
   {
-    "revision": "b8c8f7060ebd7eff4e43",
-    "url": "/swimming-stats/js/app.2eb9dd15.js"
+    "revision": "8f9c67820ed060c97ddd",
+    "url": "/swimming-stats/js/app.8eddfa06.js"
   },
   {
     "revision": "a863e4e0b250a9a26197",
@@ -28,7 +28,7 @@ self.__precacheManifest = [
     "url": "/swimming-stats/fonts/element-icons.732389de.ttf"
   },
   {
-    "revision": "f6a152d74b8deb51514bd4b5d18a99c8",
+    "revision": "a1c1bc919ed3318f4cf3279178b8d26b",
     "url": "/swimming-stats/index.html"
   },
   {
