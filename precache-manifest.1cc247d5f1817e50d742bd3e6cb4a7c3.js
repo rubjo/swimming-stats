4c46006063d2c82493f8972c0fd21e3831c229a6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-08-15 20:10:45 +0000

    Deploy rubjo/swimming-stats to github.com/rubjo/swimming-stats.git:gh-pages

---
+++ precache-manifest.1cc247d5f1817e50d742bd3e6cb4a7c3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "da14d9f17d7d51bd81cf",
+    "url": "/swimming-stats/js/about.8ec2fb71.js"
+  },
+  {
+    "revision": "c51a62a1b43795f5208e",
+    "url": "/swimming-stats/css/app.ece7c34c.css"
+  },
+  {
+    "revision": "c51a62a1b43795f5208e",
+    "url": "/swimming-stats/js/app.88c98f53.js"
+  },
+  {
+    "revision": "38a381a35066f5e29307",
+    "url": "/swimming-stats/css/chunk-vendors.d6c0cb77.css"
+  },
+  {
+    "revision": "38a381a35066f5e29307",
+    "url": "/swimming-stats/js/chunk-vendors.5c3bd9c5.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/swimming-stats/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/swimming-stats/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "eb34178e0122f926a897c8625e282fff",
+    "url": "/swimming-stats/index.html"
+  },
+  {
+    "revision": "f71d20196d4caf35b6a670db8c70b03d",
+    "url": "/swimming-stats/robots.txt"
+  }
+];
--- src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>Oppdatert Tue Sep 24 21:58:27 2019 CEST</h1>
-  </div>
-</template>
